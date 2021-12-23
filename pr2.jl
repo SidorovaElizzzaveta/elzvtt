7b3617eaf3fe16e7a67d2e04e594b0c6bed592e9
SidorovaElizzzaveta <90868365+SidorovaElizzzaveta@users.noreply.github.com>
2021-12-23 09:01:57 +0300

    Add files via upload

---
+++ pr2.jl
@@ -0,0 +1,34 @@
+function mark_perimeter!(r::Robot)
+    vert = moves!(r, Sud)
+    hor = moves!(r, West)
+   
+       for side in (Nord, Ost, Sud, West)
+           putmarkers!(r, side)
+       end 
+   
+       moves!(r, Nord, vert)
+       moves!(r, Ost, hor)
+   end
+   
+   function moves!(r::Robot,side::HorizonSide)
+       steps=0
+       while isborder(r,side)==false
+           move!(r,side)
+           steps+=1
+       end
+       return steps
+   end
+   
+   function moves!(r::Robot,side::HorizonSide,steps::Int)
+       for _ in 1:steps
+           move!(r,side)
+       end
+   end
+   
+   function putmarkers!(r::Robot, side::HorizonSide)
+       while isborder(r,side)==false
+           move!(r,side)
+           putmarker!(r)
+       end
+   end
+
